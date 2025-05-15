d28f002d70da27a82cdb63b3667b856e48f8d15f
MK-v5 <mksomna@st.noorderpoort.nl>
2025-05-15 09:58:29 +0200

    added database again.

--- database/db.inc.php
+++ database/db.inc.php
@@ -1,11 +1,11 @@
 <?php
 $servername = "localhost";
-$dbname = "uitleensysteem";
+$database = "uitleensysteem";
 $username = "root";
 $password = "";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
 } catch(PDOException $e) {
