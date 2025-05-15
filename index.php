250530c5860aa332c314d9f8cf097a2ffd4b6847
MK-v5 <mksomna@st.noorderpoort.nl>
2025-05-15 12:47:02 +0200

    added inputs partial

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 <?php
 
+    $options = [ "Exit", "Docent", "lijst" ];
+
+    function askInput()
+    {
+        global $options;
+        $i = 0;
+        echo "Please input an option\n";
+        foreach ($options as $option){
+            // EOL is End Of Line
+            echo "[" . $i++ . "]" . $option . PHP_EOL;
+        }
+        echo ">> ";
+        $input = readline();
+
+        if ($input == 0)
+        {
+            exit(0);
+        }
+        if ($input == 1)
+        {
+
+        }
+        if ($input == 2)
+        {
+            
+        }
+    }
+
